cc08bc99b5049b81fdc4a7342c3b2dd8ce679842
Pavlo Liulia <pavloblack@hotmail.com>
2016-10-19 21:15:56 +0200

    add more tests

--- test/lazyfunSpec.js
+++ test/lazyfunSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var chai = require('chai'),
   spies = require('chai-spies');
 
@@ -11,79 +13,142 @@ describe('lazyfun', function () {
     expect(lazyfun).to.be.a('function');
   });
 
-  it('should invoke callback only once', function () {
-    var callback = chai.spy();
+  describe('lazy', function () {
+    it('should invoke callback in lazy way', function () {
+      var callback = chai.spy();
 
-    var func = lazyfun(callback);
+      var func = lazyfun(callback);
 
-    expect(callback).to.not.have.been.called();
-
-    func();
-
-    expect(callback).to.have.been.called.once;
-  });
-
-  it('should invoke callback with passed parameter', function () {
-    var callback = chai.spy();
-
-    var func = lazyfun(callback, null, 'foo', 'bar');
-    func();
-
-    expect(callback).to.have.been.called.with('foo', 'bar');
-  });
-
-  it('should invoke callback after passed timeout', function (done) {
-    var callback = chai.spy();
-
-    lazyfun(callback, 10);
-
-    setTimeout(function () {
-      expect(callback).to.have.been.called.once;
-
-      done();
-    }, 11);
-  });
-
-  it('should not invoke callback before passed timeout', function (done) {
-    var callback = chai.spy();
-
-    lazyfun(callback, 10);
-
-    setTimeout(function () {
       expect(callback).to.not.have.been.called();
 
-      done();
-    }, 6);
+      func();
+
+      expect(callback).to.have.been.called.once;
+    });
+
+    it('should invoke callback with passed parameter', function () {
+      var callback = chai.spy();
+
+      var func = lazyfun(callback, null, 'foo', 'bar');
+      func();
+
+      expect(callback).to.have.been.called.with('foo', 'bar');
+    });
+
+    it('should return result from callback', function () {
+      var callbackResult = 'foo';
+
+      var func = lazyfun(function () {
+        return callbackResult;
+      });
+
+      var result = func();
+
+      expect(callbackResult).to.equal(result);
+    });
+
+    it('should invoke function returned from callback', function () {
+      var callbackResult = chai.spy();
+
+      var func = lazyfun(function () {
+        return callbackResult;
+      });
+      func();
+
+      expect(callbackResult).to.have.been.called.once;
+    });
+
+    it('should invoke function returned from callback with passed parameters', function () {
+      var callbackResult = chai.spy();
+
+      var func = lazyfun(function () {
+        return callbackResult;
+      });
+      func('foo', 'bar');
+
+      expect(callbackResult).to.have.been.called.with('foo', 'bar');
+    });
+
+    it('should not invoke callback more than once', function () {
+      var callback = chai.spy();
+
+      var func = lazyfun(callback);
+
+      for (var i = 0; i < 10; i += 1) {
+        func();
+      }
+
+      expect(callback).to.have.been.called.once;
+    });
   });
 
-  it('should invoke callback immediately', function () {
-    var callback = chai.spy();
+  describe('deferred', function () {
+    it('should invoke callback after passed timeout', function (done) {
+      var callback = chai.spy();
 
-    lazyfun(callback, -1);
+      lazyfun(callback, 10);
 
-    expect(callback).to.have.been.called.once;
+      setTimeout(function () {
+        expect(callback).to.have.been.called.once;
+
+        done();
+      }, 11);
+    });
+
+    it('should not invoke callback before passed timeout', function (done) {
+      var callback = chai.spy();
+
+      lazyfun(callback, 10);
+
+      setTimeout(function () {
+        expect(callback).to.not.have.been.called();
+
+        done();
+      }, 6);
+    });
+
+    it('should invoke callback on demand', function (done) {
+      var callback = chai.spy();
+
+      var func = lazyfun(callback, 10);
+      func();
+
+      expect(callback).to.have.been.called.once;
+
+      setTimeout(function () {
+        expect(callback).to.have.been.called.once;
+
+        done();
+      }, 15);
+    });
+
+    it('should invoke callback on demand only once', function (done) {
+      var callback = chai.spy();
+
+      var func = lazyfun(callback, 10);
+
+      for (var i = 0; i < 10; i += 1) {
+        func();
+      }
+
+      expect(callback).to.have.been.called.once;
+
+      setTimeout(function () {
+        expect(callback).to.have.been.called.once;
+
+        done();
+      }, 15);
+    });
   });
 
-  it('should invoke function returned from callback', function () {
-    var callbackResult = chai.spy();
+  describe('immediate', function () {
+    it('should invoke callback immediately', function () {
+      var callback = chai.spy();
 
-    var func = lazyfun(function () {
-      return callbackResult;
-    }, -1);
-    func();
+      lazyfun(callback, -1);
 
-    expect(callbackResult).to.have.been.called.once;
-  });
-
-  it('should invoke function returned from callback with passed parameters', function () {
-    var callbackResult = chai.spy();
-
-    var func = lazyfun(function () {
-      return callbackResult;
-    }, -1);
-    func('foo', 'bar');
-
-    expect(callbackResult).to.have.been.called.with('foo', 'bar');
+      expect(callback).to.have.been.called.once;
+    });
   });
 });
 
